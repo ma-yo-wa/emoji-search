6d9e21e1bb3b7061f431ea3c3a4e607f7cc4e610
Mayowa Adegbola <madegbola_andela@medable.com>
2023-07-13 08:52:21 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Emoji Search
+
+This application allows users to search for emojis and display the filtered results in real-time.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm i`
+
+Installs dependencies\
+
+### `npm run dev`
+
+Runs the app in the development mode.\
+
+The page will reload if you make edits.\
+
+### `npm run test`
+
+Launches the test runner in the interactive watch mode.\
--- src/tests/EmojiList.test.tsx
+++ src/tests/EmojiList.test.tsx
@@ -1,10 +1,22 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import EmojiList from "../EmojiList";
+import { EmojiContextProvider } from "../EmojiContextProvider";
 
 describe("EmojiList", () => {
   it("expects EmojiList component to be rendered", () => {
     render(<EmojiList />);
     expect(screen.getByText("Emoji Search")).toBeInTheDocument();
+  });
+
+  it("shows the correct emoji when a user searches with the appropriate search term", async () => {
+    render(
+      <EmojiContextProvider>
+        <EmojiList />
+      </EmojiContextProvider>
+    );
+    const input = screen.getByLabelText("Emoji Search");
+    fireEvent.change(input, { target: { value: "laugh" } });
+    await waitFor(() => expect(screen.getByText("🤣")).toBeInTheDocument());
   });
 });
